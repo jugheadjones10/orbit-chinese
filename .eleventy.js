9945f2e96467d234cb96c5be0eeb6bbeca461b46
Kim Young Jin <kimyoungjin1001@gmail.com>
2022-03-12 06:58:24 +0000

    first commit

---
+++ .eleventy.js
@@ -0,0 +1,13 @@
+const { EleventyServerlessBundlerPlugin } = require("@11ty/eleventy");
+
+module.exports = function(eleventyConfig) {
+   eleventyConfig.addPlugin(EleventyServerlessBundlerPlugin, {
+    name: "onrequest", // The serverless function name from your permalink object
+    functionsDir: "./netlify/functions/",
+  });
+
+  // Copy `img/` to `_site/img`
+  eleventyConfig.addPassthroughCopy("big.css");
+  eleventyConfig.addPassthroughCopy("big.js");
+
+};
